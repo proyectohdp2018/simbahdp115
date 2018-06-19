854dca57e3034d555b4739d9e2c3f64574e8376d
proyectohdp2018 <proyecto.hdp2018@gmail.com>
2018-06-18 20:48:32 -0600

    version para instancia

--- simuladorBsim/simuladorBsim/settings.py
+++ simuladorBsim/simuladorBsim/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'simuladorBsim.urls'
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'BACKEND': 'django.template.backends.mysql',
         'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
