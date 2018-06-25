362a3a8167d713bfdc266f49cb1adbce5ca994cb
proyectohdp2018 <proyecto.hdp2018@gmail.com>
2018-06-25 15:12:33 -0600

    base final

--- simuladorBsim/simuladorBsim/settings.py
+++ simuladorBsim/simuladorBsim/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'simuladorBsim.wsgi.application'
 
 DATABASES = {
    'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'mysql.connector.django',
         'NAME': 'simuladorbsim',
         'USER': 'root',
         'PASSWORD': 'Minerva',
